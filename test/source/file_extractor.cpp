d740ca221dd559a9e2cc04a21ab74a350df517f9
hoanggiapvu79116 <hoanggiapvuvhg@gmail.com>
2024-02-14 12:14:29 +1100

    Removed redundant code for testing file extractor. Fixed linking problem for test

--- test/source/file_extractor.cpp
+++ test/source/file_extractor.cpp
@@ -1,6 +1,5 @@
 #include <doctest/doctest.h>
 #include <file_extractor.hpp>
-#include <iostream>
 #include <string>
 
 TEST_CASE("Test file extractor") {
@@ -9,7 +8,6 @@ TEST_CASE("Test file extractor") {
 
   CHECK(file_extractor.getc(0) == 'T');
   CHECK(file_extractor.getc(file_extractor.find_first_of('\n', 0)) == '\n');
-  // std::cout << "First location: " << file_extractor.find_first_of('\n', 0) << '\n';
   CHECK(file_extractor.getc(file_extractor.find_last_of('h', 1000)) == 'h');
   CHECK(file_extractor.slice(5, 9) == "is t");
 }
